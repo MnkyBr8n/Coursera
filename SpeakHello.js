839475721f64e66e9632f3d83954292035a1adb6
MnkyBr8n <yxyel315@outlook.com>
2025-02-13 00:59:30 -0800

    .

---
+++ SpeakHello.js
@@ -0,0 +1,13 @@
+// Wrap in IIFE
+(function (window) {
+  var helloSpeaker = {};
+
+  var speakWord = "Hello";
+
+  helloSpeaker.speak = function (name) {
+    console.log(speakWord + " " + name);
+  };
+
+  // Expose helloSpeaker to the global scope
+  window.helloSpeaker = helloSpeaker;
+})(window);
