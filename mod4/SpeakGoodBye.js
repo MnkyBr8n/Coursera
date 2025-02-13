aa6d42ccc4287319265edb2efeb9fa77fbd6c051
MnkyBr8n <yxyel315@outlook.com>
2025-02-13 00:19:17 -0800

    .

---
+++ mod4/SpeakGoodBye.js
@@ -0,0 +1,13 @@
+// Wrap in IIFE
+(function (window) {
+  var byeSpeaker = {};
+
+  var speakWord = "Good Bye";
+
+  byeSpeaker.speak = function (name) {
+    console.log(speakWord + " " + name);
+  };
+
+  // Expose byeSpeaker to the global scope
+  window.byeSpeaker = byeSpeaker;
+})(window);
